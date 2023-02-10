f2eb4d89814d9dee47ba9b8ae7c8ae5fc0063a86
Hyunsoo <kaist217@naver.com>
2023-02-11 02:08:36 +0900

    app.js 변경, users.js 변경

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 const goodsRouter = require('./routes/goods');
 const cartsRouter = require('./routes/carts.js');
+const usersRouter = require('./routes/users');
 
 const connect = require('./schemas');
 connect();
@@ -11,7 +12,7 @@ connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('assets'));
-app.use('/api', [goodsRouter, cartsRouter]);
+app.use('/api', [goodsRouter, cartsRouter, usersRouter]);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
