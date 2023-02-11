ba82e026f631d3e09603fbf50df7a89898c68b43
Hyunsoo <kaist217@naver.com>
2023-02-11 10:07:24 +0900

    auth.js 추가

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000;
 const goodsRouter = require('./routes/goods');
 const cartsRouter = require('./routes/carts.js');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const connect = require('./schemas');
 connect();
@@ -12,7 +13,7 @@ connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('assets'));
-app.use('/api', [goodsRouter, cartsRouter, usersRouter]);
+app.use('/api', [goodsRouter, cartsRouter, usersRouter, authRouter]);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const User = require('../schemas/user');
 router.post('/users', async (req, res) => {
   const { email, nickname, password, confirmPassword } = req.body;
 
+  // 패스워드와 패스워드 확인이 성공
   if (password !== confirmPassword) {
     res.status(400).json({
       errorMessage: '패스워드가 패스워드 확인란과 다릅니다.',
@@ -13,12 +14,12 @@ router.post('/users', async (req, res) => {
     return;
   }
 
-  // email 또는 nickname이 동일한 데이터가 있는지 확인하기 위해 가져온다.
+  // email, nickname이 실제로 DB에 존재하는지 확인.
   const existsUsers = await User.findOne({
-    $or: [{ email }, { nickname }],
+    $or: [{ email }, { nickname }], // 이메일 또는 닉네임이 일치할 때, 조회한다.
   });
   if (existsUsers) {
-    // NOTE: 보안을 위해 인증 메세지는 자세히 설명하지 않습니다.
+    // NOTE: 보안을 위해 인증 메세지는 자세히 설명하지 않는다.
     res.status(400).json({
       errorMessage: '이메일 또는 닉네임이 이미 사용중입니다.',
     });
@@ -26,7 +27,7 @@ router.post('/users', async (req, res) => {
   }
 
   const user = new User({ email, nickname, password });
-  await user.save();
+  await user.save(); // DB에 저장한다.
 
   res.status(201).json({});
 });
